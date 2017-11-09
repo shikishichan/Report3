782e6b48d6f21973fefd80b68a91ff0fe294f56a
Kisho Shiraishi <e175761@ie.u-ryukyu.ac.jp>
2017-11-09 21:31:26 +0900

    final commit

--- src/main/jp/ac/uryukyu/ie/e175761/LivingThing.java
+++ src/main/jp/ac/uryukyu/ie/e175761/LivingThing.java
@@ -1,10 +1,10 @@
 package jp.ac.uryukyu.ie.e175761;
 
 public class LivingThing {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     public String getName() {
         return name;
@@ -12,6 +12,30 @@ public class LivingThing {
 
     public boolean isDead() {
         return dead;
+    }
+
+    public int getHitPoint(){
+        return hitPoint;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack =attack;
+    }
+
+    public void setDead(boolean dead) {
+        this.dead = dead;
+    }
+
+    public void setHitPoint(int maximumHP) {
+        hitPoint = maximumHP;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public LivingThing(String name, int maximumHP, int attack) {
@@ -23,7 +47,7 @@ public class LivingThing {
     }
 
     public void attack(LivingThing opponent){
-        if(dead != true) {
+        if(dead == false) {
             int damage = (int) (Math.random() * attack);
             System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
             opponent.wounded(damage);
